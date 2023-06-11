a6208b70a1f235f38ec1227ec502efc563a951dd
wewinagain <ivan.mir444@gmail.com>
2023-06-11 00:34:48 -0300

    Ejercicio 2 Hecho

---
+++ tp8Punto2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.IO;
+string rutacarpeta =@"C:\Users\User\Dropbox\Facultad\Arquitectura y Organización de Computadoras I";
+string[] archivos=Directory.GetFiles(rutacarpeta);
+string rutaarchivo=@"C:\Taller1 repositorio\tl1_tp8_2023-IvanDMir\";
+string nombre=@"index.csv";
+string nombrearchivo=rutaarchivo+nombre;
+StreamWriter guardararchivo=new StreamWriter(nombrearchivo);
+int i=0;
+foreach (var item in archivos)
+{
+    Console.WriteLine(item);
+    string[] dividirnombreguardar=item.Split(@"\");
+    string nombreguardar=dividirnombreguardar[dividirnombreguardar.Count()-1];
+    string[] dividirextension=nombreguardar.Split('.');
+    string extension=dividirextension[dividirextension.Count()-1];
+    guardararchivo.WriteLine(i+nombreguardar+extension);
+    i++;
+
+}
+guardararchivo.Close();
+
+
